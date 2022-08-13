f590b564cdcdea066d7e8f60f1b13c86f617336d
0x_raafet <109506437+0x-raafet@users.noreply.github.com>
2022-08-13 10:52:09 +0000

    Update and rename rdp.sh to rdp

--- rdp.sh
+++
@@ -1,8 +0,0 @@
-echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic universe" >> /etc/apt/sources.list
-apt-get update
-apt -y install vnc4server
-apt-get install xfce4 -y
-echo "xfce4-session" > ~/.xsession
-apt install firefox -y
-vncpasswd
-vncserver -geometry 1366x768
